6ef253a2eb19691fefe40cb40abc97c58fa29d6e
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-10-09 16:51:26 +0200

    Change word/NE types from String to Text, update the schema

--- NLP/Nerf.hs
+++ NLP/Nerf.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module NLP.Nerf
 ( train
@@ -9,55 +10,62 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Monad (forM_)
 import Control.Monad.Lazy (forM)
 import Control.Arrow (second)
-import Data.Char (toLower)
 import Data.Maybe (mapMaybe)
 import System.IO (hSetBuffering, stdout, BufferMode (..))
+import qualified Data.Char as C
 import qualified Data.Set as S
 import qualified Data.Vector as V
-import qualified Data.Text.Lazy as L
+import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as L
 
-import Control.Monad.Ox
-import Numeric.SGD (SgdArgs (..), Para, sgd, sgdM, sgd)
-import qualified Control.Monad.Ox.String as Ox
+import qualified Control.Monad.Ox as Ox
+import qualified Control.Monad.Ox.Text as Ox
 
 import Data.Named.Tree (mapForest)
 import Text.Named.Enamex (parseEnamex)
 import qualified Data.Named.Tree as Tr
 import qualified Data.Named.IOB as IOB
 
+import Numeric.SGD (SgdArgs)
 import qualified Data.CRF.Chain1 as CRF
 
-type Word = String
-type NE = String
-type Ob = ([Int], String)
-type Lb = IOB.Label String
+type Word = T.Text
+type NE = T.Text
+type Ob = ([Int], T.Text)
+type Lb = IOB.Label T.Text
 
-schema :: V.Vector Word -> Int -> Ox Word String ()
+-- | TODO: Add dictionary lookup.
+schema :: V.Vector Word -> Int -> Ox.Ox Word T.Text ()
 schema sent = \k -> do
-    mapM_ (save . lowOrth)          [k - 1, k]
-    -- mapM_ (save . upOnlyOrth)    [k - 1, k]
+    mapM_ (Ox.save . lowOrth)       [k - 1, k]
+    mapM_ (Ox.save . upOnlyOrth)    [k - 1, k]
     mapM_ lowLemma                  [k - 1, k]
-    mapM_ (save . shape)            [k - 1, k]
-    mapM_ (save . packedShape)      [k - 1, k]
-    -- etc.
+    mapM_ (Ox.save . shape)         [k - 1, k]
+    mapM_ (Ox.save . shapeP)        [k - 1, k]
+    Ox.save $ link <$> shape  k <*> shape  (k - 1)
+    Ox.save $ link <$> shapeP k <*> shapeP (k - 1)
   where
-    at              = atWith sent
+    at              = Ox.atWith sent
     orth            = (id `at`)
-    lowOrth i       = map toLower <$> orth i
-    shape i         = Ox.shape <$> orth i
-    packedShape i   = Ox.pack <$> shape i
+    lowOrth i       = T.toLower <$> orth i
+    upOnlyOrth i    = orth i >>= \x -> case T.any C.isUpper x of
+        True    -> Just x
+        False   -> Nothing
 
-    lowLemma i = group $ do
-        mapM_ (save . lowPrefix i) [0, -1, -2, -3]
-        mapM_ (save . lowSuffix i) [0, -1, -2, -3]
+    shape i         = Ox.shape <$> orth i
+    shapeP i        = Ox.pack <$> shape i
+    link x y        = T.concat [x, "-", y]
+
+    lowLemma i = Ox.group $ do
+        mapM_ (Ox.save . lowPrefix i) [0, -1, -2, -3]
+        mapM_ (Ox.save . lowSuffix i) [0, -1, -2, -3]
 
     lowPrefix i j = Ox.prefix j =<< lowOrth i
     lowSuffix i j = Ox.suffix j =<< lowOrth i
 
 schematize :: [Word] -> CRF.Sent Ob
 schematize xs =
-    map (S.fromList . execOx . schema v) [0 .. n - 1]
+    map (S.fromList . Ox.execOx . schema v) [0 .. n - 1]
   where
     v = V.fromList xs
     n = V.length v
@@ -71,14 +79,8 @@ flatten forest =
     xs = schematize (map IOB.word iob)
     ys = map IOB.label iob
 
-readData :: FilePath -> IO [CRF.SentL Ob Lb]
-readData path
-    = map flatten
-    . map (mapForest toString)
-    . parseEnamex
-   <$> L.readFile path
-  where
-    toString = Tr.onBoth (L.unpack . L.fromStrict)
+readFlat :: FilePath -> IO [CRF.SentL Ob Lb]
+readFlat path = map flatten . parseEnamex <$> L.readFile path
 
 -- | Train the CRF model.
 train
@@ -87,9 +89,9 @@ train
     -> Maybe FilePath       -- ^ Maybe eval data (ENAMEX)
     -> IO (CRF.CRF Ob Lb)   -- ^ Resulting codec and model
 train sgdArgs trainPath evalPathM = do
-    let readTrain = readData trainPath
+    let readTrain = readFlat trainPath
         readEvalM = evalPathM >>= \evalPath ->
-            Just ([], readData evalPath)
+            Just ([], readFlat evalPath)
     CRF.train sgdArgs readTrain readEvalM CRF.presentFeats
 
 -- | Tag with the CRF model.
