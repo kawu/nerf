64ed9a9bc940bc68b2ab8c3481f43031dfa5f93d
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-10-10 16:22:31 +0200

    Add basic dictionary support

--- NLP/Nerf.hs
+++ NLP/Nerf.hs
@@ -9,8 +9,11 @@ module NLP.Nerf
 ) where
 
 import Control.Applicative ((<$>), (<*>))
+import Data.Maybe (maybeToList)
+import Control.Monad (join)
 import qualified Data.Char as C
 import qualified Data.Set as S
+import qualified Data.Map as M
 import qualified Data.Vector as V
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as L
@@ -25,19 +28,23 @@ import qualified Data.Named.IOB as IOB
 import Numeric.SGD (SgdArgs)
 import qualified Data.CRF.Chain1 as CRF
 
+import qualified NLP.Nerf.Dict.LMF as LMF
+
 type Word = T.Text
 type NE = T.Text
 type Ob = ([Int], T.Text)
 type Lb = IOB.Label T.Text
 
 -- | TODO: Add dictionary lookup.
-schema :: V.Vector Word -> Int -> Ox.Ox Word T.Text ()
-schema sent = \k -> do
+schema :: LMF.NeDict -> V.Vector Word -> Int -> Ox.Ox Word T.Text ()
+schema dict sent = \k -> do
+    mapM_ (Ox.saves . searchDict)   [k - 1, k]
     mapM_ (Ox.save . lowOrth)       [k - 1, k]
     mapM_ (Ox.save . upOnlyOrth)    [k - 1, k]
     mapM_ lowLemma                  [k - 1, k]
     mapM_ (Ox.save . shape)         [k - 1, k]
     mapM_ (Ox.save . shapeP)        [k - 1, k]
+    suffixes                                k 
     Ox.save $ link <$> shape  k <*> shape  (k - 1)
     Ox.save $ link <$> shapeP k <*> shapeP (k - 1)
   where
@@ -48,9 +55,15 @@ schema sent = \k -> do
         True    -> Just x
         False   -> Nothing
 
+    searchDict i    = join . maybeToList $
+        S.toList <$> (orth i >>= flip M.lookup dict)
+
     shape i         = Ox.shape <$> orth i
     shapeP i        = Ox.pack <$> shape i
     link x y        = T.concat [x, "-", y]
+
+    suffixes i = Ox.group $ do
+        mapM_ (Ox.save . lowSuffix i) [2, 3, 4]
 
     lowLemma i = Ox.group $ do
         mapM_ (Ox.save . lowPrefix i) [0, -1, -2, -3]
@@ -59,39 +72,46 @@ schema sent = \k -> do
     lowPrefix i j = Ox.prefix j =<< lowOrth i
     lowSuffix i j = Ox.suffix j =<< lowOrth i
 
-schematize :: [Word] -> CRF.Sent Ob
-schematize xs =
-    map (S.fromList . Ox.execOx . schema v) [0 .. n - 1]
+schematize :: LMF.NeDict -> [Word] -> CRF.Sent Ob
+schematize dict xs =
+    map (S.fromList . Ox.execOx . schema dict v) [0 .. n - 1]
   where
     v = V.fromList xs
     n = V.length v
 
-flatten :: Tr.NeForest NE Word -> CRF.SentL Ob Lb
-flatten forest =
+flatten :: LMF.NeDict -> Tr.NeForest NE Word -> CRF.SentL Ob Lb
+flatten dict forest =
     [ CRF.annotate x y
     | (x, y) <- zip xs ys ]
   where
     iob = IOB.encodeForest forest
-    xs = schematize (map IOB.word iob)
+    xs = schematize dict (map IOB.word iob)
     ys = map IOB.label iob
 
-readFlat :: FilePath -> IO [CRF.SentL Ob Lb]
-readFlat path = map flatten . parseEnamex <$> L.readFile path
+readFlat :: LMF.NeDict -> FilePath -> IO [CRF.SentL Ob Lb]
+readFlat dict path = map (flatten dict) . parseEnamex <$> L.readFile path
+
+drawSent :: CRF.SentL Ob Lb -> IO ()
+drawSent sent = do
+    let unDist (x, y) = (x, CRF.unDist y)
+    mapM_ (print . unDist) sent
+    putStrLn "" 
 
 -- | Train the CRF model.
 train
     :: SgdArgs              -- ^ Args for SGD
+    -> LMF.NeDict           -- ^ LMF dictionary
     -> FilePath             -- ^ Train data (ENAMEX)
     -> Maybe FilePath       -- ^ Maybe eval data (ENAMEX)
     -> IO (CRF.CRF Ob Lb)   -- ^ Resulting codec and model
-train sgdArgs trainPath evalPathM = do
-    let readTrain = readFlat trainPath
+train sgdArgs dict trainPath evalPathM = do
+    let readTrain = readFlat dict trainPath
         readEvalM = evalPathM >>= \evalPath ->
-            Just ([], readFlat evalPath)
+            Just ([], readFlat dict evalPath)
     CRF.train sgdArgs readTrain readEvalM CRF.presentFeats
 
 -- | Tag with the CRF model.
-tag :: CRF.CRF Ob Lb -> [Word] -> Tr.NeForest NE Word
-tag crf ws = 
-    let xs = CRF.tag crf (schematize ws)
+tag :: LMF.NeDict -> CRF.CRF Ob Lb -> [Word] -> Tr.NeForest NE Word
+tag dict crf ws =
+    let xs = CRF.tag crf (schematize dict ws)
     in  IOB.decodeForest [IOB.IOB w x | (w, x) <- zip ws xs]
