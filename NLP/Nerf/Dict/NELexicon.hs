913c2dc68ce17f20a914e627d197870ed6ee5e23
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-10-12 12:21:41 +0200

    Change library name to nerf

--- NLP/Nerf/Dict/NELexicon.hs
+++ NLP/Nerf/Dict/NELexicon.hs
@@ -22,20 +22,3 @@ parseLine line = case L.break (==';') line of
 -- | Read the dictionary from the file.
 readNELexicon :: FilePath -> IO [Entry]
 readNELexicon = fmap parseNELexicon . L.readFile
-
--- Wczytujemy kilka zasobów słownikowych a następnie dzielimy je na dwie
--- kategorie: te, które chcemy używać samodzielnie oraz te które chcemy
--- wykorzystywać w połączeniu z PoliMorfem.
---
--- 1. W tym przypadku zadanie jest w miarę proste -- należy połączyć
--- zbiór słowników [M.Map Form (S.Set NeType)] w jeden słownik NeDict
--- (M.Map Form (S.Set NeType)), a następnie zapisać go w postaci
--- binarnej.
---
--- 2. Z PoliMorfem jest trochę trudniej.
---   * Najpierw wczytujemy wszystkie słowniki z tej kategorii i łączymy
---     je w jeden zasób używając funkcji merge i diff.
---   * Wczytujemy polimorfa i zamieniamy go na BaseMap.
---   * Używamy funkcji 'merge' z pakietu polimorf.
---   * Zamieniamy słownik na DAWG,
---   * Zapisujemy go w postaci binarnej.
