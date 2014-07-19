b1c1447419b5ae9865b07f46bdcb584a2a9ccecf
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2014-07-19 20:00:23 +0200

    new module for XCES support

--- src/NLP/Nerf/Dict/PNEG.hs
+++ src/NLP/Nerf/Dict/PNEG.hs
@@ -9,35 +9,44 @@ module NLP.Nerf.Dict.PNEG
 , readPNEG
 ) where
 
-import Text.XML.PolySoup
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.IO as L
 
-import NLP.Nerf.Dict.Base
+import           Text.XML.PolySoup hiding (P, Q)
+import qualified Text.XML.PolySoup as P
 
-lmfP :: XmlParser L.Text [Entry]
-lmfP = true ##> lexEntryP
+import           NLP.Nerf.Dict.Base
 
-lexEntryP :: XmlParser L.Text [Entry]
-lexEntryP = tag "LexicalEntry" `joinR` do
-    many_ $ cut $ tag "feat"
-    _words <- many wordP
-    sense  <- senseP
-    return [Entry x sense | x <- _words]
+type P a = P.P (XmlTree L.Text) a
+type Q a = P.Q (XmlTree L.Text) a
 
-wordP :: XmlParser L.Text Form
-wordP = head <$> (tag "Lemma" <|> tag "WordForm" /> featP "writtenForm")
-
-senseP :: XmlParser L.Text NeType
-senseP = head <$> (tag "Sense" //> featP "externalReference" <|> featP "label")
-
-featP :: L.Text -> XmlParser L.Text T.Text
-featP x = L.toStrict <$> cut (tag "feat" *> hasAttr "att" x *> getAttr "val")
+-- lmfP :: XmlParser L.Text [Entry]
+-- lmfP = true ##> lexEntryP
+-- 
+-- lexEntryP :: XmlParser L.Text [Entry]
+-- lexEntryP = tag "LexicalEntry" `joinR` do
+--     many_ $ cut $ tag "feat"
+--     _words <- many wordP
+--     sense  <- senseP
+--     return [Entry x sense | x <- _words]
+-- 
+-- wordP :: XmlParser L.Text Form
+-- wordP = head <$> (tag "Lemma" <|> tag "WordForm" /> featP "writtenForm")
+-- 
+-- senseP :: XmlParser L.Text NeType
+-- senseP = head <$> (tag "Sense" //> featP "externalReference" <|> featP "label")
+-- 
+-- featP :: L.Text -> XmlParser L.Text T.Text
+-- featP x = L.toStrict <$> cut (tag "feat" *> hasAttr "att" x *> getAttr "val")
+-- 
+-- -- | Parse the dictionary to the list of entries.
+-- parsePNEG :: L.Text -> [Entry]
+-- parsePNEG = parseXml lmfP
 
 -- | Parse the dictionary to the list of entries.
 parsePNEG :: L.Text -> [Entry]
-parsePNEG = parseXml lmfP
+parsePNEG = undefined
 
 -- | Read the dictionary from the file.
 readPNEG :: FilePath -> IO [Entry]
---
+++ src/NLP/Nerf/XCES2.hs
@@ -0,0 +1,297 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveFunctor #-}
+
+
+-- | Support for the XCES format (new version).
+--
+-- This is not a regular XCES data format, it provides a way to
+-- represent NEs as well.
+
+
+module NLP.Nerf.XCES2
+( 
+-- * Data types
+  XCES
+, Para
+, Sent
+, NE (..)
+, Tok (..)
+, Lex (..)
+
+-- * Parsing
+, parseXCES
+
+-- * Printing
+, showXCES
+) where
+
+
+import           Control.Applicative
+-- import           Control.Monad (void)
+import           Data.String (IsString)
+import           Data.List (intersperse)
+import qualified Data.Foldable as F
+import qualified Data.Text.Lazy as L
+
+-- import           Data.Tree
+-- import qualified Data.Named.Tree as Tr
+-- import           Data.Named.Tree (NeForest)
+import           Data.Named.Tree
+
+import           Text.StringLike (StringLike)
+import           Text.HTML.TagSoup (Tag (..))
+import qualified Text.HTML.TagSoup as TS
+
+import           Text.XML.PolySoup hiding (P, Q)
+import qualified Text.XML.PolySoup as P
+
+-- import qualified Text.NKJP.Named as NKJP
+
+
+---------------------------------------------------------------------
+-- XCES data types
+---------------------------------------------------------------------
+
+
+-- | XCES file consists of a list of paragraphs.
+type XCES t = [Para t]
+
+
+-- | Each paragraph consists of a list of sentences (there is no
+-- paragraph identifier).
+type Para t = [Sent t]
+
+
+-- | A sentence is a forest of `NE`s (kept in internal nodes)
+-- with tokens (or no-space markers) stored in leaves.
+-- No-space markers are represented with `Nothing` values.
+type Sent t = [Root t]
+
+
+-- | A root of the NE forest.
+type Root t = NeTree (NE t) (Maybe (Tok t))
+
+
+-- | A named entity (NE).  It is a higly simplified version of
+-- the definition given in the nkjp package.
+data NE t = NE
+    { neType        :: t
+    , subType       :: Maybe t
+    , derivType     :: Maybe t }
+    deriving (Show, Functor)
+
+
+-- | NE definition given in the nkjp package:
+-- data NE t = NE
+--     { neID          :: t
+--     -- | We don't specify `derivFrom` for the same reason for which
+--     -- we don't specify `base` values. 
+--     , derived       :: Maybe (Deriv t)
+--     , neType        :: t
+--     , subType       :: Maybe t
+--     -- | Can be easly calculated from tokens. 
+--     , orth          :: t
+--     -- | Left base or Right when.  Nerf doesn't output base
+--     -- forms, for which reason they are ignored.
+--     , base          :: Maybe (Either t t)
+--     -- | The rest of attributes are irrelevant from Nerf and
+--     -- full NKJP points of view.
+--     , cert          :: Maybe Cert
+--     , certComment   :: Maybe t
+--     , ptrs          :: [Ptr t] }
+--     deriving (Show)
+-- 
+-- -- | If NE is a derived entity.
+-- data Deriv t = Deriv
+--     { derivType :: t 
+--     , derivFrom :: t }
+--     deriving (Show, Functor)
+
+
+-- | A token.
+data Tok t = Tok
+    { orth  :: t
+    , lexs  :: [Lex t] }
+    deriving (Show, Functor)
+
+
+-- | A lexical entry -- a potential interpretation of a segment.
+data Lex t = Lex
+    { base      :: t
+    , ctag      :: t
+    -- | Was it chosen while disambiguating?
+    , disamb    :: Bool }
+    deriving (Show, Functor)
+
+
+-------------------------------------------------
+-- Parsing
+-------------------------------------------------
+
+
+-- TODO: Change L.Text to (Eq t).
+
+
+-- | Parse textual contents of the ann_morphosyntax.xml file.
+parseXCES :: L.Text -> XCES L.Text
+parseXCES = F.concat . evalP xcesP . parseForest . TS.parseTags
+
+
+-- | Parsing predicates.
+type P a = P.P (XmlTree L.Text) a
+type Q a = P.Q (XmlTree L.Text) a
+
+
+xcesP :: P (XCES L.Text)
+xcesP = concat <$> every' (true //> paraQ)
+
+
+paraQ :: Q (Para L.Text)
+paraQ = named "chunk" *> hasAttrVal "type" "p" /> sentQ
+
+
+sentQ :: Q (Sent L.Text)
+sentQ = named "chunk" *> hasAttrVal "type" "s" /> nodeQ
+
+
+nodeQ :: Q (Root L.Text)
+nodeQ = (flip Node [] . Right <$> segQ) <|> neTreeQ
+
+
+segQ :: Q (Maybe (Tok L.Text))
+segQ =  (Just <$> tokQ)
+    <|> (Nothing <$ nsQ)
+    where nsQ = node $ named "ns"
+
+
+tokQ :: Q (Tok L.Text)
+tokQ = named "tok" `joinR` (Tok
+    <$> first (getTxt "orth")
+    <*> every' lexQ )
+
+
+lexQ :: Q (Lex L.Text)
+lexQ = (getAttr "disamb" <$> named "lex") `join` \dmb -> ( Lex
+    <$> first (getTxt "base")
+    <*> first (getTxt "ctag")
+    <*> pure (dmb == Just "1") )
+
+
+neTreeQ :: Q (Root L.Text)
+neTreeQ =
+    mkNode <$> (neQ </> nodeQ)
+  where
+    mkNode (ne, xs) = Node (Left ne) xs
+    neQ = named "ne" *> ( NE
+        <$> attr "type"
+        <*> optional (attr "subtype")
+        <*> optional (attr "derivtype") )
+
+
+-------------------------------------------------
+-- Parsing utilities
+-------------------------------------------------
+
+
+-- | Get contexnts of the embedded text XML node.
+getTxt :: L.Text -> Q L.Text
+getTxt x = named x `joinR` first (node text)
+
+
+-------------------------------------------------
+-- Printing
+-------------------------------------------------
+
+
+-- | Convert XCES into its XML representation. 
+showXCES :: (IsString s, StringLike s) => XCES s -> s
+showXCES = TS.renderTags . renderTree . xcesToXML
+
+
+-- | Convert XCES to XML tree.
+xcesToXML :: IsString s => XCES s -> Tree (Tag s)
+xcesToXML xs = Node
+    (TagOpen "chunkList" [])
+    (sperse breakLine $ map paraToXML xs)
+
+
+paraToXML :: IsString s => Para s -> Tree (Tag s)
+paraToXML xs = Node
+    (TagOpen "chunk" [("type", "p")])
+    (sperse breakLine $ map sentToXML xs)
+
+
+sentToXML :: IsString s => Sent s -> Tree (Tag s)
+sentToXML xs = Node
+    (TagOpen "chunk" [("type", "s")])
+    (sperse breakLine $ map rootToXML xs)
+
+
+-- rootToXML :: IsString s => Root s -> Tree (Tag s)
+-- rootToXML =
+--     mapTree $ either id id . onEither f g
+--   where
+--     f NE{..} = TagOpen "ne"
+--         [ ("type", neType) ]
+--         -- , ("subtype", subType)
+--         -- , ("derivtype", derivType) ]
+--     g Nothing = TagOpen "ns" []
+--     g (Just Tok{..}) = TagText "ASDF"
+
+
+rootToXML :: IsString s => Root s -> Tree (Tag s)
+rootToXML (Node r xs) = case r of
+    Left NE{..} -> Node neTag
+        (sperse breakLine $ map rootToXML xs)
+      where
+        neTag = TagOpen "ne" $
+            [ ("type", neType) ] ++
+            mbTag "subtype" subType ++
+            mbTag "derivtype" derivType
+        mbTag x y = case y of
+            Nothing -> []
+            Just z  -> [(x, z)]
+    Right Nothing -> Node (TagOpen "ns" []) []
+    Right (Just tok) -> tokToXML tok
+    
+
+tokToXML :: IsString s => Tok s -> Tree (Tag s)
+tokToXML Tok{..} = mkNode "token" []
+    $ sperse breakLine
+    $ mkNode "orth" [] [mkText orth]
+    : map lexToXML lexs
+
+
+lexToXML :: IsString s => Lex s -> Tree (Tag s)
+lexToXML Lex{..} = mkNode "lex" atts
+    [ mkNode "base" [] [mkText base]
+    , mkNode "ctag" [] [mkText base] ]
+  where
+    atts = if disamb
+        then [("disamb", "1")]
+        else []
+
+
+-------------------------------------------------
+-- Printing utilities
+-------------------------------------------------
+
+
+mkNode :: IsString s => s -> [TS.Attribute s] -> Forest (Tag s) -> Tree (Tag s)
+mkNode x = Node . TagOpen x
+
+
+mkText :: s -> Tree (Tag s)
+mkText x = Node (TagText x) []
+
+
+breakLine :: IsString s => Tree (Tag s)
+breakLine = mkText "\n"
+
+
+-- | Similar to `intersperse`, but adds the item at the beginning
+-- and at the end of the list as well.
+sperse :: a -> [a] -> [a]
+sperse y (x:xs) = y : x : sperse y xs 
+sperse y [] = [y]
